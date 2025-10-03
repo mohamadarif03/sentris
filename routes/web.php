cc3ec7617ee32f94d01418a436dc7c902f834738
mohamadarif03 <pecintarealmadrid2@gmail.com>
2025-10-03 23:44:52 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -9,36 +9,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('admin.dashboard');
+    return view('welcome');
 });
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::view('dashboard', 'admin.dashboard')->name('dashboard');
-    Route::view('users', 'admin.users.index')->name('users.index');
-    Route::view('settings', 'admin.settings.index')->name('settings.index');
-    Route::view('logs', 'admin.logs.index')->name('logs.index');
-});
+Route::view('dashboard-admin', 'admin.dashboard')->name('admin.dashboard');
+Route::view('users', 'admin.users.index')->name('admin.users.index');
+Route::view('settings', 'admin.settings.index')->name('admin.settings.index');
+Route::view('logs', 'admin.logs.index')->name('admin.logs.index');
 
-Route::prefix('manajemen')->name('manajemen.')->group(function () {
-    Route::view('dashboard', 'manajemen.dashboard')->name('dashboard');
-    Route::view('reports', 'manajemen.reports.index')->name('reports.index');
-    Route::view('scenarios', 'manajemen.scenarios.index')->name('scenarios.index');
-});
+Route::view('dashboard-manajemen', 'manajemen.dashboard')->name('manajemen.dashboard');
+Route::view('reports', 'manajemen.reports.index')->name('manajemen.reports.index');
+Route::view('scenarios', 'manajemen.scenarios.index')->name('manajemen.scenarios.index');
 
-Route::prefix('validator')->name('validator.')->group(function () {
-    Route::view('dashboard', 'validator.dashboard')->name('dashboard');
-    Route::view('history', 'validator.history.index')->name('history.index');
-});     
+Route::view('dashboard-validator', 'validator.dashboard')->name('validator.dashboard');
+Route::view('history-validator', 'validator.history.index')->name('validator.history.index');
 
-Route::prefix('kontributor')->name('kontributor.')->group(function () {
-    Route::view('dashboard', 'kontributor.dashboard')->name('dashboard');
-    Route::view('submit', 'kontributor.submit.index')->name('submit.index');
-});
+Route::view('dashboard-kontributor', 'kontributor.dashboard')->name('kontributor.dashboard');
+Route::view('submit', 'kontributor.submit.index')->name('kontributor.submit.index');
 
-Route::prefix('keuangan')->name('keuangan.')->group(function () {
-    Route::view('dashboard', 'keuangan.dashboard')->name('dashboard');
-    Route::view('history', 'keuangan.history.index')->name('history.index');
-});
-Route::prefix('auditor')->name('auditor.')->group(function () {
-    Route::view('dashboard', 'auditor.dashboard')->name('dashboard');
-});
+Route::view('dashboard-keuangan', 'keuangan.dashboard')->name('keuangan.dashboard');
+Route::view('history-keuangan', 'keuangan.history.index')->name('keuangan.history.index');
+
+Route::view('dashboard-auditor', 'auditor.dashboard')->name('auditor.dashboard');
